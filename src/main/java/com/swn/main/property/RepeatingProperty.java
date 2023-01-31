e2e636d7eadff15c732cfb2f860e707450a39cf9
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-01-31 23:35:29 +0700

    Remove unused variable

--- src/main/java/com/swn/main/property/RepeatingProperty.java
+++ src/main/java/com/swn/main/property/RepeatingProperty.java
@@ -3,13 +3,11 @@ package com.swn.main.property;
 import com.swn.main.dice.Dice;
 
 import java.util.List;
-import java.util.Random;
 
 public class RepeatingProperty extends PropertyImpl {
 
     private final int repeating;
     private final int rollMax;
-    private final Random random = new Random();
 
     public RepeatingProperty(int repeating, int rollMax, String description, int startNumber, int endNumber) {
         super(description, startNumber, endNumber);
