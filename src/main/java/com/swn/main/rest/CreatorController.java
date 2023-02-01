af6877680ebaebc9513dcfac4f6d11cccf3fc9da
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-02-01 17:13:37 +0700

    adding create endpoints

---
+++ src/main/java/com/swn/main/rest/CreatorController.java
@@ -0,0 +1,19 @@
+package com.swn.main.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/create")
+public class CreatorController {
+
+    @GetMapping("/pc/stat-block")
+    @ResponseBody
+    public ResponseEntity<String> generatePcAttributes() {
+        return ResponseEntity.ok("This will contain the PC stat-block");
+    }
+
+}
