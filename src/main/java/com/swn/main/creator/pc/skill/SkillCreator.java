82eba50db76e8836aeb2f57973582c212b8cbb98
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-02-05 18:54:09 +0700

    create further choices for any combat items

--- src/main/java/com/swn/main/creator/pc/skill/SkillCreator.java
+++ src/main/java/com/swn/main/creator/pc/skill/SkillCreator.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class SkillCreator {
@@ -17,16 +19,20 @@ public class SkillCreator {
         String pending = skills.pending();
         if(pending != null && !pending.isEmpty()){
             if(pending.contains(" or ")){
-                followUp = Arrays.stream(pending.split(" or ")).filter(s -> skills.chosen().stream().filter(c -> c.equals(s)).count() < 2).toList();
+                followUp = Arrays.stream(pending.split(" or ")).filter(ensureNoDuplicates(skills)).toList();
             }
             if(pending.equals("Any Combat")){
-                followUp = List.of("Shoot", "Stab", "Punch");
+                followUp = Stream.of("Shoot", "Stab", "Punch").filter(ensureNoDuplicates(skills)).toList();
             }
             if(skills.chosen().contains(pending)){
                 available.remove(pending);
             }
         }
         return new FurtherChoices(pending, convertToFurtherChoice(available), convertToFurtherChoice(followUp));
+    }
+
+    private Predicate<String> ensureNoDuplicates(SkillSet skills) {
+        return s -> skills.chosen().stream().filter(c -> c.equals(s)).count() < 2;
     }
 
     private List<FurtherChoice> convertToFurtherChoice(List<String> list) {
