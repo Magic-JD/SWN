83f2b641fc42b732be1fd0bc85f44f0fa4d966a5
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-02-05 18:35:13 +0700

    create further choices for or items

--- src/main/java/com/swn/main/creator/pc/skill/FurtherChoices.java
+++ src/main/java/com/swn/main/creator/pc/skill/FurtherChoices.java
@@ -2,5 +2,5 @@ package com.swn.main.creator.pc.skill;
 
 import java.util.List;
 
-public record FurtherChoices(String toAdd, List<FurtherChoice> choices) {
+public record FurtherChoices(String toAdd, List<FurtherChoice> choices, List<FurtherChoice> followUp) {
 }
--- src/main/java/com/swn/main/creator/pc/skill/SkillCreator.java
+++ src/main/java/com/swn/main/creator/pc/skill/SkillCreator.java
@@ -2,6 +2,8 @@ package com.swn.main.creator.pc.skill;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,12 +12,21 @@ public class SkillCreator {
 
     public FurtherChoices findFurtherChoices(SkillSet skills){
         List<String> available = skills.available();
+        List<String> followUp = new ArrayList<>();
         available.remove("All Skills");
-        if(skills.pending() != null && !skills.pending().isEmpty()){
-            if(skills.chosen().contains(skills.pending())){
-                available.remove(skills.pending());
+        String pending = skills.pending();
+        if(pending != null && !pending.isEmpty()){
+            if(pending.contains(" or ")){
+                followUp = new ArrayList<>(Arrays.stream(pending.split(" or ")).filter(s -> !skills.chosen().contains(s)).toList());
+                //pending = null;
+            } else if(skills.chosen().contains(pending)){
+                available.remove(pending);
             }
         }
-        return new FurtherChoices(skills.pending(), skills.available().stream().map(s -> new FurtherChoice(s, null)).collect(Collectors.toList()));
+        return new FurtherChoices(pending, convertToFurtherChoice(available), convertToFurtherChoice(followUp));
+    }
+
+    private List<FurtherChoice> convertToFurtherChoice(List<String> list) {
+        return list.stream().map(s -> new FurtherChoice(s, null)).collect(Collectors.toList());
     }
 }
