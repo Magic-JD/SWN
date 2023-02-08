f3e54c72947d9494d0e695000cb9ed5941fab134
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-02-08 15:41:11 +0700

    Added the skills for greater ease

--- src/main/java/com/swn/main/creator/pc/foci/FociDetails.java
+++ src/main/java/com/swn/main/creator/pc/foci/FociDetails.java
@@ -1,4 +1,4 @@
 package com.swn.main.creator.pc.foci;
 
-public record FociDetails(String name, String restriction, String description, String level1, String level2) {
+public record FociDetails(String name, String restriction, String skill, String description, String level1, String level2) {
 }
